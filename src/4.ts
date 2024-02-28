96915c22e5f0c54f868ee32048ed151636130cea
SerhiiMW <137871447+SerhiiMW@users.noreply.github.com>
2024-02-28 19:36:51 +0200

    homework-3

--- src/4.ts
+++ src/4.ts
@@ -1,39 +1,39 @@
 
 class Key {
-    private signature: any;
+    private signature: number = Math.random();
 
-    getSignature() {
-        return this.signature.Math.random();
+    getSignature() : number {
+        return this.signature;
     }
   }
 
 class Person {
    constructor(private key : Key) {}
 
-   getKey() {
+   getKey() : Key {
         return this.key;
     }
   }
 
 
 abstract class House {
-    public door: true | false;
-    public tenants: Array<number> = [];
-    constructor(key : Key) {}
+    protected door: boolean = false;
+    private tenants: Array<Person> = [];
+    constructor(protected key : Key) {}
 
-    comeIn(person) {
-    if(this.door = true) {
-        this.tenants.push(person)
+    comeIn(Person) : void {
+    if(this.door === true) {
+        this.tenants.push(Person)
     }    
     }
-    public abstract openDoor(key : {}) : void;
+    public abstract openDoor(key : Key) : void;
 
 
 }
 
 class MyHouse extends House {
-    openDoor(key : {}) : void {
-        if(key === key) {
+    openDoor(key : Key) : void {
+        if(key.getSignature() === this.key.getSignature()) {
             this.door = true;
         }
     }
