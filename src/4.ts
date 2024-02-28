6eb3126c52074cefebe35d2765783bec6d747138
SerhiiMW <137871447+SerhiiMW@users.noreply.github.com>
2024-02-28 17:41:16 +0200

    homework-3

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,44 @@
+
+class Key {
+    private signature: any;
+
+    getSignature() {
+        return this.signature.Math.random();
+    }
+  }
+
+class Person {
+   constructor(private key : Key) {}
+
+   getKey() {
+        return this.key;
+    }
+  }
+
+
+abstract class House {
+    public door: true | false;
+    public tenants: Array<number> = [];
+    constructor(key : Key) {}
+
+    comeIn(person) {
+    if(this.door = true) {
+        this.tenants.push(person)
+    }    
+    }
+    public abstract openDoor(key : {}) : void;
+
+
+}
+
+class MyHouse extends House {
+    openDoor(key : {}) : void {
+        if(key === key) {
+            this.door = true;
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
